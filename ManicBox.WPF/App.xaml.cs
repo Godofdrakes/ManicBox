7adda1895f01ce1d5cd6d337a5b768de8b4cd55f
Godofdrakes <Godofdrakes@gmail.com>
2024-04-03 12:22:54 -0700

    WPF project setup

--- ManicBox.WPF/App.xaml.cs
+++ ManicBox.WPF/App.xaml.cs
@@ -9,4 +9,19 @@ namespace ManicBox.WPF;
 /// </summary>
 public partial class App : Application
 {
+	private readonly IServiceProvider _serviceProvider;
+
+	public App( IServiceProvider serviceProvider )
+	{
+		// Needed because App isn't the main entry point anymore
+		InitializeComponent();
+
+		_serviceProvider = serviceProvider;
+	}
+
+	private void App_OnStartup( object sender, StartupEventArgs e )
+	{
+		this.MainWindow = new MainWindow();
+		this.MainWindow.Show();
+	}
 }
---
+++ ManicBox.WPF/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using Dapplo.Microsoft.Extensions.Hosting.Wpf;
+using Microsoft.Extensions.Hosting;
+using ReactiveUI;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
+
+namespace ManicBox.WPF;
+
+public static class Program
+{
+	public static void Main( string[] args )
+	{
+		var host = Host.CreateDefaultBuilder(args)
+			.ConfigureWpf<App>()
+			.Build();
+
+		host.InitializeSplat()
+			.Run();
+	}
+
+	private static IHostBuilder ConfigureWpf<T>( this IHostBuilder hostBuilder )
+		where T : Application
+	{
+		hostBuilder.ConfigureWpf(builder =>
+		{
+			builder.UseApplication<T>();
+		});
+
+		hostBuilder.ConfigureServices(services =>
+		{
+			// Splat needs all this for reasons
+			services.UseMicrosoftDependencyResolver();
+
+			var locator = Locator.CurrentMutable;
+			locator.InitializeSplat();
+			locator.InitializeReactiveUI();
+		});
+
+		hostBuilder.UseWpfLifetime();
+		
+		return hostBuilder;
+	}
+
+	private static IHost InitializeSplat( this IHost host )
+	{
+		// Splat needs this for reasons
+		host.Services.UseMicrosoftDependencyResolver();
+
+		return host;
+	}
+}
