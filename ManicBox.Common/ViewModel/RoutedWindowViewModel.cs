8d67a6fdc4c6aa6ecebcf83820f2ba2de617eacc
Godofdrakes <Godofdrakes@gmail.com>
2024-06-04 12:03:13 -0700

    RoutedWindow cleanup

--- ManicBox.Common/ViewModel/RoutedWindowViewModel.cs
+++ ManicBox.Common/ViewModel/RoutedWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using ManicBox.Common.Services;
+﻿using System.Reactive;
+using ManicBox.Common.Services;
 using ReactiveUI;
 
 namespace ManicBox.Common.ViewModel;
@@ -7,15 +8,23 @@ public class RoutedWindowViewModel : WindowViewModel
 {
 	public IScreen Screen { get; }
 
-	public RoutedWindowViewModel()
-	{
-		Screen = new ScreenRoutingService();
-	}
+	public ReactiveCommand<IRoutableViewModel, IRoutableViewModel> Reset { get; }
+	public ReactiveCommand<IRoutableViewModel, IRoutableViewModel> Push { get; }
+	public ReactiveCommand<Unit, IRoutableViewModel> Pop { get; }
+
+	public RoutedWindowViewModel() : this( new ScreenRoutingService() ) { }
 
 	public RoutedWindowViewModel( IScreen screen )
 	{
 		ArgumentNullException.ThrowIfNull( screen );
 
 		Screen = screen;
+
+		Reset = ReactiveCommand.CreateFromObservable( ( IRoutableViewModel viewModel ) =>
+			Screen.Router.NavigateAndReset.Execute( viewModel ) );
+		Push = ReactiveCommand.CreateFromObservable( ( IRoutableViewModel viewModel ) =>
+			Screen.Router.Navigate.Execute( viewModel ) );
+		Pop = ReactiveCommand.CreateFromObservable( () =>
+			Screen.Router.NavigateBack.Execute() );
 	}
 }
